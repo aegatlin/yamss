d2f75aad4ed7d845a9aa21e631ae7e1cdece0a65
Austin Gatlin <austin@gatlin.io>
2021-07-03 21:38:31 +1000

    Check if paqs is installed before attempting to install it.

--- dist/yamss.sh
+++ dist/yamss.sh
@@ -168,10 +168,13 @@ nvim__setup() {
   ensure_command git
   ensure_command npm
 
-  git clone --depth=1 https://github.com/savq/paq-nvim.git \
-    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+  if ! [ -d "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs ]; then
+    git clone --depth=1 https://github.com/savq/paq-nvim.git \
+      "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+  fi
 
   # should come _after_ asdf__setup, which installs nodejs (ie, node, npm, etc.)
+  # no harm in calling it even if it's already present
   npm install -g tree-sitter-cli
 }
 
